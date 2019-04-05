c31de1c4022716ec03b5dfd8d41e6f9e1794352d
Carlos Alberto Savi <carlos.savi@itau-unibanco.com.br>
2019-04-05 12:59:53 -0300

    Initial Commit

---
+++ SDKFake/SDKFake.h
@@ -0,0 +1,19 @@
+//
+//  SDKFake.h
+//  SDKFake
+//
+//  Created by Carlos A. Savi on 05/04/2019.
+//  Copyright © 2019 Carlos A. Savi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKFake.
+FOUNDATION_EXPORT double SDKFakeVersionNumber;
+
+//! Project version string for SDKFake.
+FOUNDATION_EXPORT const unsigned char SDKFakeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKFake/PublicHeader.h>
+
+
